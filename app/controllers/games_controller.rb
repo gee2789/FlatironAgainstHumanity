d0ae20be3836f6f809dfe892f3f82ef576ce1a0d
gee2789 <geeyung.li@gmail.com>
2017-03-27 13:08:27 -0400

    created game routes and show page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,19 @@
 class GamesController < ApplicationController
 
-  def new
-
-  end
-
   def create
+    @game = Game.create(game_params)
+    redirect_to game_path(@game)
   end
 
   def show
+    @game = Game.find_by(id: params[:id])
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,9 @@ Rails.application.routes.draw do
 	get '/', to: 'application#index', as: 'root'
 	get '/login', to: 'accounts#login', as: 'login'
   resources :activegames
-<<<<<<< HEAD
-<<<<<<< HEAD
-  resources :accounts
-  resources :games, only: [:new, :create, :show]
-=======
-=======
-
-  resources :games, only: [:new, :create, :show]
-
->>>>>>> 0e0969f8063b3cc4bcd7f8114681b2d56bc7ecbd
+  resources :games, only: [:create, :show]
   resources :accounts, only: [:new, :create]
   resources :sessions, only: [:create, :destroy]
-<<<<<<< HEAD
->>>>>>> 3f7c400e0bdbdc75475e18452f7c79dfc9404019
-=======
->>>>>>> 0e0969f8063b3cc4bcd7f8114681b2d56bc7ecbd
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
