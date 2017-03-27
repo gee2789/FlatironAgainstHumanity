4c6969028c6e7d691fbc5cb49fdbf00688d557c4
gee2789 <geeyung.li@gmail.com>
2017-03-27 16:19:30 -0400

    increment order count when activegame object is created

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,11 +2,14 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.create(game_params)
+    @activegame = Activegame.create(game_id: @game.id, account_id: current_account)
     redirect_to game_path(@game)
   end
 
   def show
     @game = Game.find_by(id: params[:id])
+    @new_deck = Deck.new
+    binding.pry
   end
 
   private
